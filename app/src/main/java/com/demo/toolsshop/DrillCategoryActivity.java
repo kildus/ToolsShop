7aea6f4ac137f0fe1bf8abf066d6a20c343ecc14
Ildus <mutator@bk.ru>
2019-07-18 01:17:13 +0300

    Initial commit

---
+++ app/src/main/java/com/demo/toolsshop/DrillCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.demo.toolsshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrillCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drill_category);
+    }
+}
---
+++ app/src/main/java/com/demo/toolsshop/MainActivity.java
@@ -0,0 +1,42 @@
+package com.demo.toolsshop;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listViewTools;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        listViewTools = findViewById(R.id.listViewTools);
+        listViewTools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+             //   Toast.makeText(getApplicationContext(), "Позиция: " + position, Toast.LENGTH_SHORT).show();
+                switch (position){
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(), DrillCategoryActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+
+            }
+        });
+
+    }
+}
